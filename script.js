365bb7f213a42cbbdf3ea33c05c0b3766337dfb9
Yangji <yangjuhyolmo13@gamil.com>
2026-02-27 21:47:38 +0545

    Feat: add initial js

---
+++ script.js
@@ -0,0 +1,34 @@
+let url = "https://pokeapi.co/api/v2/pokemon/";
+
+$(document).ready(function () {
+    $("#searchBtn").click(function () {
+        let name = $("#searchInput").val();
+
+        $.ajax(
+        {
+            url: url + name,
+            method: "GET",
+            success: success,
+            error: error
+        })
+    })
+})
+
+function success(data){
+   let pokemonHTML = `
+                    <div class="card">
+                        <h2>${data.name.toUpperCase()}</h2>
+                        <img src="${data.sprites.front_default}" />
+                        <p><strong>Height:</strong> ${data.height}</p>
+                        <p><strong>Weight:</strong> ${data.weight}</p>
+                        <p><strong>Type:</strong> ${data.types[0].type.name}</p>
+                    </div>
+                `;
+
+                $("#result").html(pokemonHTML);
+}
+
+function error(){
+   console.log(error);
+   
+}
